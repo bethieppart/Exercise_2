49be2f3691298436de56b9e105a5d312dfb6fa23
bethieppart <bethieppart@gmail.com>
2016-04-09 11:40:33 -0700

    Update db_setup.sh

--- db_setup.sh
+++ db_setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #mount data directory
-mount -t ext4 /dev/xvdf /data
+mount -t ext4 /dev/xvdg /data
+
+#switch to python 2.7
+source /opt/py27environment/bin/activate
 
 #start hadoop and postgres
 /data/start_postgres.sh
